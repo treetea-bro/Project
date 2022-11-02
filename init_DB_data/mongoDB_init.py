f3c8a2321e6bab2c9b033a9024e6fd35cd689393
seungmin lee <sorpwjdafh@naver.com>
2022-11-02 17:25:45 +0900

    dependancy lock & mongoDB_init.py

--- init_DB_data/mongoDB_init.py
+++ init_DB_data/mongoDB_init.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
+import json
 from pymongo import MongoClient
 
 mongo_connect = MongoClient("localhost", 27017)
 db = mongo_connect.Final
 
 try:
-    db.board.insert_many()
+    with open("mongoDB_Collection_board.txt", "r") as f:
+        for v in f.read().split("\n"):
+            json_object = json.loads(v)
+            db.board.insert_one(json_object)
+    with open("mongoDB_Collection_analysis.txt", "r") as f:
+        for v in f.read().split("\n"):
+            json_object = json.loads(v)
+            db.analysis.insert_one(json_object)
 except Exception as e:
     print(e)
